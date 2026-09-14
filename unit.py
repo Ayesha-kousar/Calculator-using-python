72b129f0386758beee6642bd44aeffec585b1101
Ayesha Kousar <aj4476639@gmail.com>
2026-09-14 18:10:46 +0500

    first commit

---
+++ unit.py
@@ -0,0 +1,12 @@
+
+# Assingment no 2:Unit convert 
+
+x= float (input("enter any number:"))
+print("Your answer in meters:", x*1000)
+print("Your answer in kilometers:", x/1000)
+print("Your answer in centimeters:", x*2.54)
+print("Your answer in inches:", x/2.54)
+print("Your answer in meters:", x*0.3048)
+print("Your answer in feet:", x/0.3048)
+print("Your answer in koilometer:", x*1.60934)
+print("Your answer in miles:", x/1.60934)
